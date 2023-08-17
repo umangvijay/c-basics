3a2bd0f850408f70751ab1f9a8a9f0a066f12f05
umangvijay <142140992+umangvijay@users.noreply.github.com>
2023-08-17 09:55:06 +0530

    while.c

--- while.c
+++ while.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1,number=0,b=9
-    
-    {
-        printf("enter a number:");
-        scanf("%d" &number);
-        while(i<=10){
-            printf("%d\n",(number*1));
-            i++;  
-        }
+    int i=1,number=0,b=9;
+    printf("Enter a number :");
+    scanf("%d",&number);
+    while (i<=10){
+        printf("%d\n",number*i);
+        i++;
+
     }
-     return 0;
+
+    return 0;
 }
-     
