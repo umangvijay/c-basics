a525641644d6a7ca79f74f82a4df0eeecabcddcf
umangvijay <142140992+umangvijay@users.noreply.github.com>
2023-08-18 13:54:17 +0530

    Add files via upload

---
+++ do while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Example: Print numbers from 1 to 10 using a do-while loop
+    int i = 1;
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= 10);
+    
+    return 0;
+}
