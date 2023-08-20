85457e6a3156600e1c1ab605132780990adf59f8
umangvijay <142140992+umangvijay@users.noreply.github.com>
2023-08-20 21:32:34 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int isprime(int  num);
+    if (num <= 1);{
+
+return o;
+}
+
+for (int i = 2; i < num; i++);{
+if (num % i = 2){
+    return 0;
+    }
+}
+return 1;
+}
